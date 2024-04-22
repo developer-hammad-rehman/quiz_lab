34fff5eece929f52d82db5d4e103dc38e60bfdec
Hammad-Rehman <124163466+developer-hammad-rehman@users.noreply.github.com>
2024-04-23 00:01:03 +0500

    first

--- quiz_app_frontend/src/app/page.tsx
+++ quiz_app_frontend/src/app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { BackgroundBoxes } from './components/Hero'
+import { AnimatedPin } from './components/cards'
 
 export default function Home() {
   return (
-    <div>
+    <div className='overflow-x-hidden bg-gray-900'>
       <BackgroundBoxes/>
+      <h4 className='text-center text-5xl font-bold text-white py-7'>Topices</h4>
+      <AnimatedPin/>
     </div>
   )
 }
