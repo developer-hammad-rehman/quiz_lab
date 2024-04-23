33fc4683c459d2dc91df6e6de8063df88f9c993e
Hammad-Rehman <124163466+developer-hammad-rehman@users.noreply.github.com>
2024-04-24 00:53:17 +0500

    main

--- quiz_app_frontend/src/app/components/Navbar.tsx
+++ quiz_app_frontend/src/app/components/Navbar.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <header className='py-8 px-7 flex justify-between bg-purple-300'>
-        <h1 className='text-4xl font-bold text-yellow-200'>Quizii Hub</h1>
+    <header className='py-8 px-7 flex justify-between bg-purple-300 '>
+        <h1 className='text-xl  sm:text-4xl font-bold text-yellow-200'>Quizii Hub</h1>
         <div className='flex gap-6'>
-            <Link  className='bg-gradient-to-tr from-red-400 to-yellow-200 text-white py-3 px-7 rounded-lg' href={'/login'}>Sigin</Link>
-            <Link className='bg-gradient-to-tr from-red-400 to-yellow-200 text-white py-3 px-7 rounded-lg' href={'/register'}>Sigin UP</Link>
+            <Link  className='bg-gradient-to-tr from-red-400 to-yellow-200 text-white py-2 px-2  sm:py-3 sm:px-7 rounded-lg' href={'/login'}>Sigin</Link>
+            <Link className='bg-gradient-to-tr from-red-400 to-yellow-200 text-white py-2 px-2 sm:py-3 sm:px-7 rounded-lg' href={'/register'}>Sigin UP</Link>
         </div>
     </header>
   )
